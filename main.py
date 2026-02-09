256325bd170044839458c63375458afd0846ca98
datcoding <dat3052005@gmail.com>
2026-02-09 22:38:05 +0700

    add

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from argparse import ArgumentParser
 from configs.utils import get_config
 from dataset.data_utils import Preprocessor
 from tasks import train, test
+import random
+import numpy as np
+import torch
+import os
 
 parser = ArgumentParser()
 parser.add_argument("--config-file", type=str, required=True)
@@ -10,6 +14,17 @@ config_file = args.config_file
 
 if __name__ == "__main__":
     config = get_config(config_file)
+
+    SEED = config.seed
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+    os.environ["PYTHONHASHSEED"] = str(SEED)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
     Preprocessor(config.dataset).run()
     train(config)
     test(config)
