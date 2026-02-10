19ef90020ebe0eb70533ffdbc1126bd5f0ee9a18
datcoding <dat3052005@gmail.com>
2026-02-10 16:05:55 +0700

    add

--- models/NN.py
+++ models/NN.py
@@ -10,19 +10,41 @@ class NeuralNetwork(nn.Module):
         self.c = config.channels
         self.input_size = self.w * self.h * self.c
         self.output_size = kwargs.get("labels", 2)
-        self.fc1 = nn.Linear(self.input_size, config.hidden_size)
-        self.fc2 = nn.Linear(config.hidden_size, self.output_size)
+        
+        # Giả sử bạn muốn dùng cùng một hidden_size cho cả 3 lớp
+        hidden = config.hidden_size
+        
+        # Khai báo 3 lớp ẩn
+        self.fc1 = nn.Linear(self.input_size, hidden)
+        self.fc2 = nn.Linear(hidden, hidden)
+        self.fc3 = nn.Linear(hidden, hidden)
+        
+        # Lớp đầu ra (output layer)
+        self.fc_out = nn.Linear(hidden, self.output_size)
+        
         self.dropout = nn.Dropout(config.dropout)
 
     def forward(self, x):
-        # input [B, C, H, W] -> flatten -> [B, C*H*W]
+        # Flatten: [B, C, H, W] -> [B, C*H*W]
         x = x.reshape(x.size(0), -1)
-        x = self.dropout(F.relu(self.fc1(x)))
-        x = self.fc2(x)
+        
+        # Lớp 1
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        
+        # Lớp 2
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        
+        # Lớp 3
+        x = F.relu(self.fc3(x))
+        x = self.dropout(x)
+        
+        # Lớp đầu ra
+        x = self.fc_out(x)
         return x
         
     def predict(self, x):
         logits = self.forward(x)
         preds = torch.argmax(logits, dim=1)
         return preds
-        
